175f1f0a6cc946388ef77c83b1e704fd41c7920e
Kiaravd <kiarayoselinventuradiaz@gmail.com>
2024-10-19 17:04:32 -0300

    Ejercicio 11

---
+++ ejer11.2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejer11._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> animales = new List<string>();
+            animales.Add("Delfin");
+            animales.Add("León");
+            animales.Add("Canguro");
+            animales.Add("Pelicano");
+            animales.Add("Rinoceronte");
+            Console.WriteLine("Ingrese un nombre");
+            string nombre = Console.ReadLine();
+            animal anim = new animal(nombre);
+
+            anim.insertar(animales);
+            anim.leer();
+
+        }
+        public class animal
+        {
+            public animal(string nom) { nombreNuevo = nom; }
+            string nombreNuevo;
+            string ruta = "C:\\prueba\\";
+            string archivo = "animales.txt";
+            public void insertar( List <string> nombres)
+            {
+                int index = 0;
+                for(int i = 0; i < nombres.Count(); i++)
+                {
+                    if (nombres[i] == "León")
+                    {
+                        index = i; break;
+                    }
+                }
+                nombres.Insert(index + 1, nombreNuevo);
+
+                StreamWriter objtxt;
+                objtxt = File.CreateText(ruta + archivo);
+
+                foreach(string nom in nombres)
+                {
+                    objtxt.WriteLine(nom);
+                }
+                objtxt.Close();
+                Console.WriteLine("Archivo creado y escrito");
+
+            }
+            public void leer() {
+                StreamReader objtxt;
+                objtxt = File.OpenText(ruta + archivo);
+
+                string linea = objtxt.ReadLine();
+                while (linea != null)
+                {
+                    Console.WriteLine(linea);
+                    linea = objtxt.ReadLine();
+                }
+                objtxt.Close();
+                Console.WriteLine("Archivo cerrado");
+
+            }
+        }
+    }
+}
---
+++ ejer11/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace ejer11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcion = 0;
+            Agregarnom agregarnom = new Agregarnom();
+
+            do
+            {
+                Console.WriteLine("Que desea hacer con el archivo?");
+                Console.WriteLine("Darle de alta: marque 1");
+                Console.WriteLine("Leerlo: marque 2");
+                Console.WriteLine("Agregar o apendizar: marque 3");
+                Console.WriteLine("salir: marque 4");
+
+                opcion = Convert.ToInt32(Console.ReadLine());
+                switch (opcion)
+                {
+                    case 1:
+                        List<string> palabras = new List<string>();
+                        Console.WriteLine("Cuantas palabras deseas agregar?");
+                        int cant = Convert.ToInt32(Console.ReadLine());
+                        for (int i = 0; i < cant; i++)
+                        {
+                            Console.WriteLine(" Palabra {0}: ", i + 1);
+                            palabras.Add(Console.ReadLine());
+                        }
+                        agregarnom.darAlta(palabras);
+                        break;
+                    case 2:
+                        agregarnom.leer();
+                        break;
+                    case 3:
+                        List<string> palabras2 = new List<string>();
+                        Console.WriteLine("Cuantas palabras deseas agregar?");
+                        int cant2 = Convert.ToInt32(Console.ReadLine());
+                        for (int i = 0; i < cant2; i++)
+                        {
+                            Console.WriteLine(" Palabra {0}: ", i + 1);
+                            palabras2.Add(Console.ReadLine());
+                        }
+                        agregarnom.escribir(palabras2);
+                        break;
+                    case 4:
+                        Console.WriteLine("Saliendo..");
+                        break;
+                    default:
+                        Console.WriteLine("Ingrese un valor correcto");
+                        break;
+                }
+            } while (opcion != 4);
+        }
+
+        public class Agregarnom
+        {
+            public Agregarnom() { }
+            public string nombre;
+            string ruta = "C:\\prueba\\";
+            string archivo = "programando.txt";
+            public void darAlta(List<string> nombres) {
+                StreamWriter objtext;
+                objtext = File.CreateText(ruta + archivo);
+                for (int i = 0; i < nombres.Count; i++)
+                {
+                    objtext.WriteLine(nombres[i]);
+                }
+                objtext.Close();
+                Console.WriteLine("Escrito y cerrado");
+            }
+            public void leer() {
+                string linea;
+                StreamReader objtext;
+                try
+                {
+                    objtext = File.OpenText(ruta + archivo);
+                    linea = objtext.ReadLine();
+                    while (linea != null) { Console.WriteLine(linea); linea = objtext.ReadLine(); }
+                    objtext.Close();
+                }
+                catch (Exception e){ Console.WriteLine(e.Message); }
+                
+            }
+            public void escribir(List <string> nombres) {
+
+                StreamWriter objtext;
+                objtext = File.AppendText (ruta + archivo);
+
+                for (int i = 0; i < nombres.Count; i++)
+                {
+                    objtext.WriteLine(nombres[i]);
+                }
+                objtext.Close();
+                Console.WriteLine("Palabra/s agregadas");
+            }
+        }
+    }
+}
